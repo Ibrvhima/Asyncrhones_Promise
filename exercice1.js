ec5058f1a0477cf5258f99296071481799971c7c
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-11-23 18:24:44 +0000

    chagement de nom script.js en exercice1

---
+++ exercice1.js
@@ -0,0 +1,13 @@
+
+//Exercice 1 : Création basique d'une Promise
+//Créez une fonction `delayedMessage` qui retourne une Promise qui se résout après 2 secondes avec le message "Bonjour!".
+function delayedMessage() {
+     return new Promise((resolve) => {
+         setTimeout(() => {
+             resolve("Bonjour!");
+         }, 2000); 
+     });
+ }
+ delayedMessage().then((message) => {
+     console.log(message); 
+ });
